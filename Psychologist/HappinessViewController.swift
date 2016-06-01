2506254f5ef74f64e8bf156b26a90ab46630e8a1
Justine Gartner <GartnerJustine@gmail.com>
2016-05-31 21:45:57 -0400

    Set up PsycologistVC and added HappinessVC

---
+++ Psychologist/HappinessViewController.swift
@@ -0,0 +1,56 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Justine Kay on 5/29/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    var happiness: Int = 100
+        { // 0 = super sad face, 100 = crazy happy face
+        
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.datasource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: Selector("scale:")))
+            //faceView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "changeHappiness"))
+        }
+    }
+    
+        private func updateUI() {
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness - 50) / 50
+    }
+}
---
+++ Psychologist/PsychologistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by Justine Gartner on 5/31/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
